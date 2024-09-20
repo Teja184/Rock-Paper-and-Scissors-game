3fa9a93314ee0cfca3b3cccc2e57e9c8574a0c43
Tejavardhan Reddy <150679222+Teja184@users.noreply.github.com>
2024-09-20 15:02:19 +0530

    Add files via upload

---
+++ rock game.py
@@ -0,0 +1,30 @@
+import random
+u=True
+def game(u): 
+    user=input("enter rock or paper or scissor : ")
+    ch=["rock","paper","scissor"]
+    comp=random.choice(ch) 
+    print("user choice : ",user)
+    print("computer choice : ",comp)  
+    if(user==comp):
+        print("It's a tie")
+    elif(user=="rock" and comp=="paper"):
+        print("Lose")
+    elif(user=="rock" and comp=="scissor"):
+        print("Win")
+    elif(user=="paper" and comp=="rock"):
+        print("Win")
+    elif(user=="paper" and comp=="scissor"):
+        print("Lose")
+    elif(user=="scissor" and comp=="rock"):
+        print("Lose")
+    elif(user=="scissor" and comp=="paper"):
+        print("Win")
+    a=input("If you want to play type yes other wise no : ")
+    if a=="yes":
+        game(u)
+    elif a=="no":
+        print("game end")
+game(u)
+
+
